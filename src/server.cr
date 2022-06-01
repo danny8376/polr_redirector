a5568225c85e017e36aad72d28aba66ba90907cb
DannyAAM <danny@saru.moe>
2022-06-02 06:22:00 +0800

    fix cache policy header

--- src/server.cr
+++ src/server.cr
@@ -117,6 +117,10 @@ def do_redirect(env)
   end
 end
 
+static_headers do |response, filepath, filestat|
+  response.headers.add("Cache-Control", "no-cache")
+end
+
 get "/:short_url" do |env|
   do_redirect env
 end
